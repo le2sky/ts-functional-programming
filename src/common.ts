dc90283a64e8328f9a1d19c26c622592e785f457
leesky <leehaneul0623@gmail.com>
2022-06-24 21:41:24 +0900

    docs: Try map 구현

---
+++ src/common.ts
@@ -0,0 +1,20 @@
+export const curry2 =
+  <A, B, C>(f: (a: A, b: B) => C) =>
+  (a: A) =>
+  (b: B): C =>
+    f(a, b);
+
+export const flip =
+  <A, B, C>(f: (a: A, b: B) => C) =>
+  (b: B, a: A): C =>
+    f(a, b);
+
+// Array<A> == A[]
+// map :: (Array<A>, (A => B)) => Array<B>
+export const map = <A, B>(array: Array<A>, f: (a: A) => B): Array<B> => {
+  const result: Array<B> = [];
+  for (const value of array) {
+    result.push(f(value));
+  }
+  return result;
+};
